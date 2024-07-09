910a434396b283b2adfc15e92502185418c6319b
injenus <injenus777@gmail.com>
2024-07-09 13:02:41 +0300

    add videos saving

--- raspberry/save_videos.py
+++ raspberry/save_videos.py
@@ -1,103 +1,82 @@
-import cv2
+
+from picamera2 import Picamera2
 import time
+import cv2
 
-def list_connected_cameras(max_cameras=10):
-    cameras = []
-    for i in range(max_cameras):
-        cap = cv2.VideoCapture(i, cv2.CAP_V4L2)
-        if cap.isOpened():
-            cameras.append(i)
-            cap.release()
-    return cameras
 
-def main():
-    # Сканируем доступные камеры
-    cameras = list_connected_cameras()
-    if len(cameras) < 2:
-        print("Недостаточно камер подключено. Найдено только:", len(cameras))
-        return
+wh_sign, wh_road = (1280, 720), (640, 360)
+cam_road = Picamera2(1)
+cam_sign = Picamera2(0)
+camera_config = cam_road.create_preview_configuration(main={"format": "BGR888", "size": wh_road})
+cam_road.configure(camera_config)
+camera_config = cam_sign.create_preview_configuration(main={"format": "BGR888", "size": wh_sign})
+cam_sign.configure(camera_config)
+cam_road.set_controls({
+        "AwbEnable": 0,  # Отключение автоматического баланса белого
+        "ColourGains": (1.4, 1.5),  # Установка коэффициентов цветовой коррекции
+        "AeEnable": 0,  # Отключение автоматической экспозиции
+        "AnalogueGain": 2.0,  # Установка аналогового усиления (чувствительности)
+        "ExposureTime": 35000  # Установка выдержки в микросекундах (например, 20000 = 20мс)
+    })
+cam_sign.set_controls({
+        "AwbEnable": 0,  # Отключение автоматического баланса белого
+        "ColourGains": (1.4, 1.5),  # Установка коэффициентов цветовой коррекции
+        "AeEnable": 0,  # Отключение автоматической экспозиции
+        "AnalogueGain": 2.0,  # Установка аналогового усиления (чувствительности)
+        "ExposureTime": 35000  # Установка выдержки в микросекундах (например, 20000 = 20мс)
+    })
+cam_sign.start()
+cam_road.start()
 
-    print("Подключенные камеры:", cameras)
 
-    # Настройки для первой камеры
-    camera1_id = cameras[0]
-    camera1_output = 'camera1_output.mp4'
-    cap1 = cv2.VideoCapture(camera1_id, cv2.CAP_V4L2)
-    cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-    cap1.set(cv2.CAP_PROP_FPS, 20)
-    fps1_set = cap1.get(cv2.CAP_PROP_FPS)
-    print(f'Настроенный FPS для камеры 1: {fps1_set}')
+fps = 20
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Кодек для записи видео
+out_road = cv2.VideoWriter('out_road.mp4', fourcc, fps, wh_road)
+out_sign = cv2.VideoWriter('out_sign.mp4', fourcc, fps, wh_sign)
 
-    # Настройки для второй камеры
-    camera2_id = cameras[1]
-    camera2_output = 'camera2_output.mp4'
-    cap2 = cv2.VideoCapture(camera2_id, cv2.CAP_V4L2)
-    cap2.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-    cap2.set(cv2.CAP_PROP_FPS, 20)
-    fps2_set = cap2.get(cv2.CAP_PROP_FPS)
-    print(f'Настроенный FPS для камеры 2: {fps2_set}')
 
-    # Кодек и настройки записи
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out1 = cv2.VideoWriter(camera1_output, fourcc, 20.0, (1280, 720))
-    out2 = cv2.VideoWriter(camera2_output, fourcc, 20.0, (1280, 720))
+prev_time_road = time.time()
+fps_road = 0
+prev_time_sign = time.time()
+fps_sign = 0
 
-    prev_time1 = time.time()
-    prev_time2 = time.time()
-    frame_count1 = 0
-    frame_count2 = 0
-    fps1 = 0
-    fps2 = 0
 
-    while cap1.isOpened() and cap2.isOpened():
-        ret1, frame1 = cap1.read()
-        ret2, frame2 = cap2.read()
-        
-        if ret1 and ret2:
-            # Рассчитываем FPS для камеры 1
-            frame_count1 += 1
-            current_time1 = time.time()
-            if current_time1 - prev_time1 >= 1:
-                fps1 = frame_count1 / (current_time1 - prev_time1)
-                frame_count1 = 0
-                prev_time1 = current_time1
-            
-            # Рассчитываем FPS для камеры 2
-            frame_count2 += 1
-            current_time2 = time.time()
-            if current_time2 - prev_time2 >= 1:
-                fps2 = frame_count2 / (current_time2 - prev_time2)
-                frame_count2 = 0
-                prev_time2 = current_time2
+while True:
+    frame_road = cam_road.capture_array()
+    frame_road = cv2.rotate(frame_road, cv2.ROTATE_180)
+    out_road.write(frame_road)
+    
+    current_time_road = time.time()
+    elapsed_time_road = current_time_road - prev_time_road
+    prev_time_road = current_time_road
+    fps_road = int(1 / elapsed_time_road)
+    print(f'ROAD {fps_road} fps')
+    cv2.imshow('Road', frame_road)
+#######
+    frame_sign = cam_sign.capture_array()
+    frame_sign = cv2.rotate(frame_sign, cv2.ROTATE_180)
+    out_sign.write(frame_sign)
+    
+    current_time_sign = time.time()
+    elapsed_time_sign = current_time_sign - prev_time_sign
+    prev_time_sign = current_time_sign
+    fps_sign = int(1 / elapsed_time_sign)
+    print(f'SIGN {fps_sign} fps')
+    cv2.imshow('Sign', frame_sign)
 
-            # Добавляем FPS на кадр
-            #cv2.putText(frame1, f'Set FPS: {fps1_set:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            cv2.putText(frame1, f'Real FPS: {fps1:.2f}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            #cv2.putText(frame2, f'Set FPS: {fps2_set:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            cv2.putText(frame2, f'Real FPS: {fps2:.2f}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-            out1.write(frame1)
-            out2.write(frame2)
+cam_road.stop()
+cam_sign.stop()
+out_road.release()
+out_sign.release()
+cv2.destroyAllWindows()
 
-            # Уменьшаем кадры в 4 раза для отображения
-            small_frame1 = cv2.resize(frame1, (320, 180))
-            small_frame2 = cv2.resize(frame2, (320, 180))
 
-            cv2.imshow('Camera 1', small_frame1)
-            cv2.imshow('Camera 2', small_frame2)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
-            break
 
-    cap1.release()
-    cap2.release()
-    out1.release()
-    out2.release()
-    cv2.destroyAllWindows()
 
-if __name__ == "__main__":
-    main()
+
+
+
